36d07ddfe211b388384291e97ff9b6a14d45fde3
buildscore-champ <84140796+buildscore-champ@users.noreply.github.com>
2021-05-13 21:14:44 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,47 @@
+# text => string  "" ''
+# num1 = int(input("number 1\n"))
+# num2 = int(input("number 2\n"))
+
+# w3schools
+# sololearn
+
+# if num1+num2>12:
+#   print("sum is more than 12")
+# elif num1+num2>10:
+#   print("sum is more than 10")
+# else :
+#   print("sum is less than 10")
+
+# variable = value 
+# car = "bmw"
+# car = "mercedes"
+# # data types
+# # python : string , boolean , integer, float
+# show = "wwe"
+# # working = "True"
+# working = True
+# working = False
+# # int(input())
+# number = 213
+# number2 = 2123
+
+# # if working==True:
+# #   print("i am  working")
+
+# print(number+number2)
+# print(show)
+# print(working)
+#bolean se mujhe bolean algebra yaaad aa gaya
+# boolean true false
+
+# print(car)
+# ctrl + /
+# Calculator : 
+
+
+num = int(input("numb"))
+num2 = int(input("numb"))
+
+operation = input("opertaion")
+
+# if operation == "add" : print(num + num2)
